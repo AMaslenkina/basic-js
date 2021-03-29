2f2581db324822c6520379ee600d858f94eb2cf9
AMaslenkina <nastya2003380@gmail.com>
2021-03-29 16:05:22 +0200

    feat: add DepthCalculator class

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,15 @@
+
+H
+test/what-season.test.js,0\d\0d7d7fedeefc9167156e0b1e8d9b19e861a3e469
+B
+src/what-season.js,a\d\adb7d660294b0c2d5ccbb4e38f60ecd150343638
+A
+src/dream-team.js,2\f\2fba50c3f2fd3166ca84e2f2c4987746dedd6ce3
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+<
+README_RU.md,9\5\954f66d490794d5fefafd7b240bf76965a3bfe1e
+L
+test/transform-array.test.js,a\8\a85200b67cc1e76a3f46e69b87d1049567cfc26c
+F
+src/transform-array.js,5\e\5e183228d90329ef5983531f8c03d6dc46e66c59
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let count = 1;
+    let res = 0;
+    arr.forEach((e) => {
+      if (Array.isArray(e)) {
+        count = this.calculateDepth(e);
+        if (res < count) {
+          res = count;
+        }
+      }
+    })
+
+    res += 1;
+    return res;
   }
 };
